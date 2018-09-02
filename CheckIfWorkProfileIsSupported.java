7a64e8a8df8591c1399bb3c0c492d1a350b07d22
AsdethPrime <asdethprime@gmail.com>
2018-09-02 14:13:27 +1000

    Create CheckIfWorkProfileIsSupported.java

---
+++ CheckIfWorkProfileIsSupported.java
@@ -0,0 +1,18 @@
+// This program checks if the device supports creating Work Profile
+
+import android.content.pm.PackageManager ;
+
+public class CheckIfWorkProfileIsSupported
+{
+	// Class consists of 1 function that return 1 value
+	// Value returned is TRUE if creating Work Profile is possible
+	// Value returned is FALSE if not possible
+
+	public static Boolean main ( String args[] )
+	{
+		Boolean isSupported ;
+		PackageManager pm = getPackageManager()
+		isSupported = pm.hasSystemFeature(PackageManager.FEATURE_MANAGED_USERS);
+		return isSupported ;
+	}
+}		
