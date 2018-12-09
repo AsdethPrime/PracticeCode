caa9d6858ff2f983acf31ca4cb0fb66cc25c5d57
AsdethPrime <asdethprime@gmail.com>
2018-12-10 09:54:59 +1100

    Create printing_arguments.js

---
+++ nodejs/printing_arguments.js
@@ -0,0 +1,14 @@
+// This is a simple javascript function that print any arguments presented to it
+// This makes uses of a built in variable called arguments that holds all arguments passed to a function
+
+// Step 1: Defining the function
+
+function args()
+{
+	console.log(arguments);
+	return 0
+}
+
+// Step 2: Calling the function
+
+args(1,2,3,4,5,6,7,8,9,10,'a','b');
