f76254a0e755832c2bc79cc47b78283e39c0f34b
AsdethPrime <asdethprime@gmail.com>
2018-12-09 23:39:53 +1100

    Update fuctions.js

--- nodejs/fuctions.js
+++ nodejs/fuctions.js
@@ -7,3 +7,13 @@ function sum(x,y)
 // Using a function 
 console.log("The sum of 3 and 7 is: " + sum(3,7));
 
+// a function that takes simply print "You have been hijacked" 100 times
+
+function thug_life()
+{
+  for ( i = 0; i<=100; i++)
+  {
+    console.log("You have been hijacked by thug_life");
+  }
+}
+
